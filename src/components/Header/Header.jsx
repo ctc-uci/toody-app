b0220aeeffc397fddb571a094dd74f0897ed2ae1
Myron Zhou <myronzhou0@gmail.com>
2022-10-25 21:44:17 -0700

    Added toody logo and link to profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-// import toody from '../../images/toody.svg';
+import toody from '../../images/toody.svg';
+import {Link} from "react-router-dom";
 // ^^ this is the Toody logo; uncomment he above line to use it
 import './Header.css';
 
@@ -11,7 +12,16 @@ const Header = () => {
         "navbar" that spans the top of the page and contains the 'Toody" logo
         at the top left and the "Profile" link at the top right. The logo
         has already been imported for you (see line 2). The completed header
-        should route the user to the Profile page correctly. */}
+        should route the user to the Profile page correctly. */
+        <><img src={toody} alt="Toody Logo"></img>
+        <div className="myProfileLink">
+          <Link to={"/Profile"}> My Profile </Link>
+        </div>
+        <div className="logoText">
+          <p>Toody</p>
+        </div>
+        </>
+        }
     </div>
   );
 };
