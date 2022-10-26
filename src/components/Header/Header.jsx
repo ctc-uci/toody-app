1af293773e6d608f9b9085dc66d215e08f4b0d87
Kory Zhang <koryiskool@yahoo.com>
2022-10-26 11:37:24 -0700

    task #2 header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-// import toody from '../../images/toody.svg';
+import toody from '../../images/toody.svg';
+import { Link } from 'react-router-dom';
 // ^^ this is the Toody logo; uncomment he above line to use it
 import './Header.css';
 
 const Header = () => {
   return (
     <div className="header">
-      {/*  TODO:
-        You will need to complete this component. The header is the
-        "navbar" that spans the top of the page and contains the 'Toody" logo
-        at the top left and the "Profile" link at the top right. The logo
-        has already been imported for you (see line 2). The completed header
-        should route the user to the Profile page correctly. */}
+      <div>
+        <img style={{ position: 'absolute', top: '40%', left: '4%' }} src={toody} />
+      </div>
+      <p style={{ position: 'absolute', top: '18%', left: '7.5%', fontSize: '25px' }}>Toody</p>
+      <Link
+        style={{
+          position: 'absolute',
+          marginLeft: '90%',
+          top: '48%',
+          textDecoration: 'none',
+          color: 'white',
+        }}
+        to="/profile"
+      >
+        My Profile
+      </Link>
     </div>
   );
 };
